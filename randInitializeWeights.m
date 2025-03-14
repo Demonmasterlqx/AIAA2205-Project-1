b23cc4abf2e986e0095efb47f858c6e70b4edde4
Demonmasterlqx <183842220@qq.com>
2025-03-14 16:01:10 +0800

    完成 randInitializeWeights

--- randInitializeWeights.m
+++ randInitializeWeights.m
@@ -19,12 +19,8 @@ W = zeros(L_out, 1 + L_in);
 % Note: The first column of W corresponds to the parameters for the bias unit
 %
 
-
-
-
-
-
-
+% 输出这个矩阵
+W = rand(L_out, 1 + L_in) * 2 * 0.12 - 0.12;
 
 
 % =========================================================================
