0afb748bfb51506369e765b4c70e608fbbab1458
fansedeguang <xli849@connect.hkust-gz.edu.cn>
2025-03-17 11:54:37 +0800

    Update randInitializeWeights.m

--- randInitializeWeights.m
+++ randInitializeWeights.m
@@ -17,7 +17,7 @@ W = zeros(L_out, 1 + L_in);
 %               training the neural network.
 % Randomly initialize the weights to small values
 epsilon init = 0.12;
-W = rand(L out, 1 + L in) * 2 * epsilon init 􀀀 epsilon init;
+W = rand(L out, 1 + L in) * 2 * epsilon init - epsilon init;
 % Note: The first column of W corresponds to the parameters for the bias unit
 %
 
