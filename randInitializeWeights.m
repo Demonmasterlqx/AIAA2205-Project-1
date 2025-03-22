dc9c35173695d7de8a4872a31c9ea37a2f2bfee0
fansedeguang <xli849@connect.hkust-gz.edu.cn>
2025-03-22 12:27:09 +0800

    Update randInitializeWeights.m

--- randInitializeWeights.m
+++ randInitializeWeights.m
@@ -16,8 +16,8 @@ W = zeros(L_out, 1 + L_in);
 % Instructions: Initialize W randomly so that we break the symmetry while
 %               training the neural network.
 % Randomly initialize the weights to small values
-epsilon init = 0.12;
-W = rand(L out, 1 + L in) * 2 * epsilon init - epsilon init;
+epsilon_init = 0.12;
+W = rand(L_out, 1 + L_in) * 2 * epsilon_init - epsilon_init;
 % Note: The first column of W corresponds to the parameters for the bias unit
 %
 
